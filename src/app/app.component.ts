bdc9181c75be89c2ad237907a155775677a62164
BrunoPogianMallta <brunopmalltaads@gmail.com>
2022-10-22 20:21:16 -0300

    add: started project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'car-sales-front';
+  title = "Bem vindo ao Curso de Angular"
 }
